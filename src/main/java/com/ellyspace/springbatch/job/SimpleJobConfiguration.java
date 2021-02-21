22348bb8b2525094d0196c25cff76c1c1346aabb
yebink <yebin825@gmail.com>
2021-02-21 17:48:49 +0900

    feat: 주석 추가

--- src/main/java/com/ellyspace/springbatch/job/SimpleJobConfiguration.java
+++ src/main/java/com/ellyspace/springbatch/job/SimpleJobConfiguration.java
@@ -1,11 +1,13 @@
-package com.ellyspace.springbatch;
+package com.ellyspace.springbatch.job;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,15 +25,30 @@ public class SimpleJobConfiguration {
     @Bean
     public Job simpleJob() {
         return jobBuilderFactory.get("simpleJob") //simpleJob이라는 이름으로 batch job을 생성한다.
-                .start(simpleStep1())
+                .start(simpleStep1(null))
+                .next(simpleStep2(null))
                 .build();
     }
 
     @Bean
-    public Step simpleStep1() {
+    @JobScope
+    public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) { //jobParameters : 외부에서 받을 수 있는 파라미터 (Program Arguments)
         return stepBuilderFactory.get("simpleStep1") //simpleStep1이라는 batch step 생성
                 .tasklet(((contribution, chunkContext) -> { //tasklet: step 안에서 단일로 수행될 커스텀 기능들을 선언할 때 사용
                     log.info(">>>>> This is STEP1");
+                    log.info(">>>>> requestDate = {}" , requestDate);
+                    return RepeatStatus.FINISHED;
+                }))
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step simpleStep2(@Value("#{jobParameters[requestDate]}") String requestDate) {
+        return stepBuilderFactory.get("simpleStep2")
+                .tasklet(((contribution, chunkContext) -> {
+                    log.info(">>>>> This is STEP2");
+                    log.info(">>>>> requestDate = {}", requestDate);
                     return RepeatStatus.FINISHED;
                 }))
                 .build();
@@ -50,7 +67,22 @@ public class SimpleJobConfiguration {
  * - 최근 실패한 Batch Parameter가 어떤 것들이고, 성공한 Job은 어떤 것들이 있는지
  * - 다시 실행한다면 어디서부터 시작하면 될지
  * - 어떤 Job에 어떤 Step들이 있었고, Step들 중에서 성공한 Step과 실패한 Step들은 어떤 것들이 있는지
- *
+ */
+
+/**
  * MySQL이랑 연결 참고: https://jojoldu.tistory.com/325
  *
+ * [BATCH_JOB_INSTANCE]
+ * Job Parameter(배치 실행 시 외부에서 받을 수 있는 파라미터)에 따라 생성되는 테이블.
+ * 방금 실행했던 Job의 정보 저장. Job Parameter가 다르면 같은 Batch Job이더라도 두 번 기록되지 않는다.
+ *
+ * [BATCH_JOB_EXECUTION]
+ * JOB_INSTANCE가 성공/실패했던 모든 내역을 갖고 있다.
+ * JOB_INSTANCE와 부모-자식 관계
+ *
+ * BatchStatus vs ExitStatus
+ * BatchStatus는 job 또는 step의 실행 결과를 Spring에서 기록할 때 사용하는 Enum
+ * ExitStatus는 step 실행 후의 상태 표시
+ *
+ * jobParameters는 @JobScope, @StepScope 일 때만 받아올 수 있다.
  */
